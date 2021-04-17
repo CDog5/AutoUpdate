b1c8bc7df2426b946cbaff154717fd872cd89d50
CDog5 <callum.sheppard78@btinternet.com>
2021-04-17 18:45:39 +0100

    Delete Simple Lexer directory

--- Simple Lexer/v1/Example.py
+++
@@ -1,9 +0,0 @@
-from lexer import Lexer 
-
-lexer = Lexer()
-lexed = lexer.lex("2.2 + 2")
-print(lexed.as_raw())
-print(lexed.as_tuple())
-print(lexed.as_list())
-print(lexed.as_dict())
-lexer = Lexer()
